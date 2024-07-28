778d57e5f8ccab1b6c485cf4d5ac6dfd7ad4e38a
slojararshavin <sunday@tm30.net>
2024-07-28 19:19:32 +0100

    add requirements

--- loanapp/settings.py
+++ loanapp/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = env('SECRET_KEY', None)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["tutwe.com", "www.tutwe.com"]
 
 # Application definition
 
